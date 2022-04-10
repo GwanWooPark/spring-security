3b8f4bfb9a2e5302575802d1bfb30a9f9b204836
GwanWooPark <pgw4712@icloud.com>
2022-04-10 21:35:48 +0900

    corespringsecurity

--- corespringsecurity/src/main/java/io/security/corespringsecurity/respository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package io.security.corespringsecurity.respository;
-
-import io.security.corespringsecurity.domain.Account;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<Account, Long> {
-
-    Account findByUsername(String username);
-}
